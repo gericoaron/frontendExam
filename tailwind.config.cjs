55ed4b34aac6c257d0e57213e0d5eb29fe6d6c99
Gerico Mangali <mangaligericoaron@gmail.com>
2022-10-04 15:02:31 +0800

    commit

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,7 +7,21 @@ module.exports = {
     
   ],
   theme: {
-    extend: {},
+    screens: {     'mobile': '400px',
+  },
+  
+    
+    colors: {
+      'body': {
+        100: '#0d3880',
+      },
+      'white':{
+        100: '#ffffff',
+      },
+      'sub':{
+        100: '#ffc600',
+      }
+    },
   },
   plugins: [
     require("tailwindcss-animate"),
